d597bf6ece83e345529ab05e6a2d3795be5b6967
dbm3tz <dbm3tz@virginia.edu>
2017-01-29 16:38:31 -0500

    Third commit.

---
+++ test/com/dbrianmiller/arpahealth/server/DummyFile.java
@@ -0,0 +1,13 @@
+package com.dbrianmiller.arpahealth.server;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DummyFile {
+
+	@Test
+	public void AssertT() {
+		assertTrue(true);
+	}
+}
